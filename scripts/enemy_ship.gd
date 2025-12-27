c72fee20aab2746d99326b9cb911e409c36d08c1
James Spears <jamesspearsv@gmail.com>
2025-12-26 21:43:14 -0500

    Fix enemy ship spawn position bug
    Next -> Set up collision logic between lasers and enemy ships

--- scripts/enemy_ship.gd
+++ scripts/enemy_ship.gd
@@ -5,18 +5,14 @@ signal ememy_hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	linear_velocity = Vector2.DOWN * speed
 	pass # Replace with function body.
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _physics_process(delta: float) -> void:
-	#var velocity = Vector2.DOWN
-	#velocity = velocity * speed
-	#position += velocity * delta
+func _physics_process(delta: float) -> void:
+	pass
 
 
 func _on_body_entered(body: Node) -> void:
 	print('enemy hit!')
 	emit_signal('ememy_hit')
-	pass # Replace with function body.
--- scripts/main.gd
+++ scripts/main.gd
@@ -7,7 +7,6 @@ var laser_cooling_down: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$PlayerShip.position = $StartingMarker.position
-	add_enemy_ship()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -24,16 +23,13 @@ func _process(_delta: float) -> void:
 
 
 func add_enemy_ship() -> void:
-	print(
-		'Adding enemy ship!'
-	)
 	var enemy_ship: RigidBody2D = enemy_scene.instantiate()
-	
 	var spawn_pos = $EnemyPath/EnemySpawnLocation
 	spawn_pos.progress_ratio = randf()
-	#print(spawn_pos.progress_ratio)
 	
-	enemy_ship.position = spawn_pos.position
+	enemy_ship.linear_velocity = Vector2(0.0, randf_range(100.00, 400.00))
+	
+	enemy_ship.position = Vector2(spawn_pos.position.x, spawn_pos.position.y)
 	enemy_ship.linear_velocity = Vector2.DOWN * 1000
 	add_child(enemy_ship)
 
@@ -44,5 +40,3 @@ func _on_laser_cooldown_timeout() -> void:
 
 func _on_enemy_spawn_timer_timeout() -> void:
 	add_enemy_ship()
-
-	
